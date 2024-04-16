c8b1c81d21af70952f08a255793fe376607d3ebf
Alon Niv <oakfang@gmail.com>
2024-04-17 01:07:05 +0300

    initial code

---
+++ README.md
@@ -0,0 +1,7 @@
+# Get this running
+
+- Download [NodeJS](https://nodejs.org/en)
+- Clone this repo (or download it)
+- run `npm i`
+- run `npx playwright install`
+- run `npm start`
---
+++ index.js
@@ -0,0 +1,48 @@
+import { LiveChat } from "youtube-chat";
+import { chromium } from "playwright";
+import { setTimeout } from "node:timers/promises";
+import pokemon from "./pokemon.json" assert { type: "json" };
+
+const QUIZ_URL = "https://pkmnquiz.com/";
+const CHAT_ID = "9TMslV0kvcM";
+const POKEMON_REGEXP = new RegExp(
+  `(${pokemon.join("|").replace(/\./g, "\\.")})`,
+  "ig"
+);
+
+const liveChat = new LiveChat({ liveId: CHAT_ID });
+const browser = await chromium.launch({ headless: false });
+const page = await browser.newPage();
+await page.goto(QUIZ_URL);
+await setTimeout(3000);
+await page.click("#gen0");
+const pokemonInput = page.locator("#pokemon");
+
+// Emit at receive chat.
+// chat: ChatItem
+liveChat.on("chat", async ({ message }) => {
+  const text = message
+    .map((m) => m.text)
+    .filter(Boolean)
+    .join(" ");
+  if (!text) return;
+  const pokemonInMessage = text.match(POKEMON_REGEXP);
+  if (pokemonInMessage) {
+    for (let name of pokemonInMessage) {
+      await pokemonInput.fill(name);
+      await pokemonInput.press("Enter");
+    }
+  }
+});
+
+// Emit when an error occurs
+// err: Error or any
+liveChat.on("error", (err) => {
+  console.error(err);
+  liveChat.stop();
+});
+
+const ok = await liveChat.start();
+if (!ok) {
+  console.log("Failed to start, check emitted error");
+}
